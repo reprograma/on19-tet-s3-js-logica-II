43565f98b42c0bffdc70394f63a471217f3dc0eb
Karoline Amaral <karolinearaujo852@gmail.com>
2022-08-31 19:03:20 -0300

    atualizando

--- exercicios/para-casa/04-desafios-opcionais/04-desafio-final.js
+++
@@ -1,6 +0,0 @@
-/*
-  Tente criar um programa que junte o máximo possível de tudo
-  o que vimos essa semana! Tente junta pegar input de usuário,
-  argumentos de linha de comando, condicionais e funções em algo
-  bem louco! E não esqueça de compartilhar com todo mundo.
-*/
